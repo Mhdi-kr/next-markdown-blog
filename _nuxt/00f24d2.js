06eeff1a40e468101ecd79710fbd6eaf4e7ee315
Mahdi Karimi <mhdi_kr@outlook.com>
2021-03-01 09:51:39 +0330

    98  redsign

--- _nuxt/00f24d2.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{296:function(t,e,n){"use strict";n.r(e);var c=n(24),component=Object(c.a)({},(function(){var t=this.$createElement;this._self._c;return this._m(0)}),[function(){var t=this.$createElement,e=this._self._c||t;return e("section",[e("p",[this._v("s")])])}],!1,null,null,null);e.default=component.exports}}]);

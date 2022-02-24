cde039dc3bd99c493b573a6e24133b709865440b
Mahdi Karimi <mhdi_kr@outlook.com>
2022-02-24 22:47:15 +0330

    publish

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function Header() {
-    return (
-        <footer className="bg-dark-blog text-center p-2">
-            <span>2019 - {new Date().getFullYear()}</span>
-        </footer>
-    );
-}
--- interfaces/IPost.ts
+++
@@ -1,7 +0,0 @@
-export interface IPost {
-    id: number;
-    title: string;
-    content: string;
-    imagePath: string;
-    slug: string;
-}

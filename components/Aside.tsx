571ba82b54173076ebd35f3d3cdd5c6b6eb93323
Mahdi Karimi <mhdi_kr@outlook.com>
2022-03-17 02:57:38 +0330

    fix: update aside styling

--- components/Aside.tsx
+++ components/Aside.tsx
@@ -3,25 +3,40 @@ export const Aside = (props: { className: string }) => {
         <>
             <aside className={props.className}>
                 <div className="mb-4">
-                    <h2 className="font-medium mb-2 text-gray-600">
+                    <p className="font-medium mb-1 text-gray-600">
                         5 Recent posts
-                    </h2>
+                    </p>
                     <ul className="text-gray-500">
-                        <li>Developer&apos;s Procrastination</li>
-                        <li>Memory management in Arduino</li>
-                        <li>Refactoring to Functional paradigm</li>
-                        <li>Frontend Development Skilltree</li>
-                        <li>Memory management in Arduino</li>
+                        <li>
+                            <small>Developer&apos;s Procrastination</small>
+                        </li>
+                        <li>
+                            <small>Memory management in Arduino</small>
+                        </li>
+                        <li>
+                            <small>Refactoring to Functional paradigm</small>
+                        </li>
+                        <li>
+                            <small>Frontend Development Skilltree</small>
+                        </li>
+                        <li>
+                            <small>Memory management in Arduino</small>
+                        </li>
                     </ul>
                 </div>
                 <div className="mb-4">
-                    <h2 className="font-medium mb-2 text-gray-600">Tags</h2>
+                    <p className="font-medium mb-2 text-gray-600">Tags</p>
                     <ul className="text-gray-500 flex flex-wrap">
                         {Object.values(Tags)
                             .map((tag) => tag)
                             .sort((a, b) => b.length - a.length)
                             .map((tag, index) => (
-                                <li className="mr-1 mb-1 text-xs bg-gray-100 p-1 rounded cursor-pointer" key={index}>{tag}</li>
+                                <li
+                                    className="mr-1 mb-1 text-xs bg-gray-100 p-1 rounded cursor-pointer"
+                                    key={index}
+                                >
+                                    {tag}
+                                </li>
                             ))}
                     </ul>
                 </div>
